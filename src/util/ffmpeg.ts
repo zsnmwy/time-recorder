2550c92705809f5317bf205fc85bcf7662fd983a
zsnmwy <35299017+zsnmwy@users.noreply.github.com>
2024-12-31 21:25:39 +0000

    fix: increase probesize

--- src/util/ffmpeg.ts
+++ src/util/ffmpeg.ts
@@ -50,8 +50,8 @@ export const startRecorder = async (key: string, display: number, option: { widt
 
   try {
     const params = [
-      '-probesize', '6000M',
-      '-analyzeduration', '6000M',
+      // '-probesize', '6000M',
+      '-analyzeduration', '3000M',
       '-vsync', '2',
       '-use_wallclock_as_timestamps',
       '1',
